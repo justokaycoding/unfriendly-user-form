3079854a0f0b9e742afe38dd27f4c9a3ab65c1e8
hines57 <59494195+justokaycoding@users.noreply.github.com>
2021-09-03 16:12:09 -0500

    base upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php ?>
+
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Unfriendly UX/UI</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600&amp;display=swap" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <div class="content-wrapper" style="padding-top: 239.516px;">
+
+    <main role="main">
+      <div id="content">
+        <div id="" class="content_wrap">
+          <div id="" class="section">
+            <div class="constraint halign-left valign-top">
+              <div id="" class="column width-12">
+                <div class="wrapper text">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </main>
+  </div>
+  <script src="js.js"></script>
+</body>
+
+</html>
